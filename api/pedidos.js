5f148a7cc70a35ffd2efc934fa156bca36b6a25b
nicholas-apolinario03 <50881682+nicholas-apolinario03@users.noreply.github.com>
2025-05-28 11:38:52 -0300

    Update pedidos.js

--- api/pedidos.js
+++ api/pedidos.js
@@ -1,4 +1,14 @@
 export default async function handler(req, res) {
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   const { order_id } = req.query;
     console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 
