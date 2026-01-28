af30cac9a52c2124a522d1165d2120a6286fa08a
nicholas-apolinario03 <50881682+nicholas-apolinario03@users.noreply.github.com>
2026-01-27 22:24:20 -0300

    Update trocarcode.js

--- api/trocarcode.js
+++ api/trocarcode.js
@@ -1,26 +1,23 @@
 import axios from "axios";
 
 export default async function handler(req, res) {
+  try {
+    if (req.method !== "POST") {
+      return res.status(405).json({ error: "Método não permitido" });
+    }
+
     const { code } = req.body;
+
+    if (!code) {
+      return res.status(400).json({ error: "Code não recebido" });
+    }
 
     console.log("Code recebido:", code);
 
-    res.status(200).json({ ok: true });
+    return res.status(200).json({ ok: true });
+
+  } catch (err) {
+    console.error("Erro na function:", err);
+    return res.status(500).json({ error: "Erro interno" });
+  }
 }
-const clientId = '3704242181199025';
-const clientSecret = '6BE7sVvWH8u2BGam4DITcxusN8R0YMD3';
-const redirectUri = 'https://nicholas-apolinario03.github.io/testecode.html';
-const meuBotao = document.getElementById("meu-botao");
-
-const tokenResponse = await axios.post('https://api.mercadolibre.com/oauth/token', null, {
-    params: {
-        grant_type: 'authorization_code',
-        client_id: clientId,
-        client_secret: clientSecret,
-        redirect_uri: redirectUri,
-        code: id
-    }
-});
-
-const { access_token, refresh_token } = tokenResponse.data;
-console.log(tokenResponse.data);
