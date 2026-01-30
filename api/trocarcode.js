b2f4cacd33264f26eb90645cfb532b23944934b0
nicholas-apolinario03 <50881682+nicholas-apolinario03@users.noreply.github.com>
2026-01-30 20:07:52 -0300

    Update trocarcode.js

--- api/trocarcode.js
+++ api/trocarcode.js
@@ -16,22 +16,6 @@ export default async function handler(req, res) {
 
     return res.status(200).json({ ok: true });
 }
-const CLIENT_ID = process.env.CLIENT_ID;
-const CLIENT_SECRET = process.env.CLIENT_SECRET;
-const REDIRECT_URI = process.env.REDIRECT_URI;
-
-const tokenResponse = await axios.post('https://api.mercadolibre.com/oauth/token', null, {
-    params: {
-        grant_type: 'authorization_code',
-        client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET,
-        redirect_uri: REDIRECT_URI,
-        code: code
-    }
-});
-const { access_token, refresh_token } = tokenResponse.data;
-return res.status(200).json({tokenResponse});
-
 
 
 
