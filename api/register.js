57237080ffa0c7825c8e65866b930a580e9d8be8
Nicholas Apolinário <nicholas.apolinario@outlook.com>
2026-02-02 01:31:53 -0300

    Adicionar backend dos registrs

--- api/register.js
+++ api/register.js
@@ -1,5 +1,5 @@
 import bcrypt from "bcryptjs";
-import { db } from "./db.js";
+import { db } from "./bd.js";
 
 export default async function handler(req, res) {
 
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
   }
 
   if (req.method !== "POST") {
-    return res.status(405).json({ error: "Método não permitido" });
+    return res.status(405).json({ error: "Método não permitiopópp´p´p´´do" });
   }
 
   try {
