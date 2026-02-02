73fcdf6203099fd617ea5d2000ea919dad21ffe9
Nicholas Apolinário <nicholas.apolinario@outlook.com>
2026-02-02 14:17:49 -0300

    modificando build

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom ';
-import Index from './pages/Index ';
-import Register from './pages/Register ';
+import Index from './pages/Index';
+import Register from './pages/Register';
 
 function App() {
   return (
