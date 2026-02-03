2fba6270d0b34c90ecbbd6c895aded4197fcff9c
Nicholas Apolinário <nicholas.apolinario@outlook.com>
2026-02-03 16:23:03 -0300

    botao de criar no dashboard

--- Frontend/src/pages/Dashboard.jsx
+++ Frontend/src/pages/Dashboard.jsx
@@ -135,7 +135,7 @@ export default function Dashboard() {
                 </div>
             </div>
             <br />
-            <Link to='/novo-produto'></Link>
+            <Link to='/novo-produto'>Novo Produto</Link>
             <button onClick={handleLogout}>Sair</button>
         </div>
     );
