98c599182b5020cfba1487f6c694ae43546f446c
Nicholas Apolinário <nicholas.apolinario@outlook.com>
2026-02-10 15:53:30 -0300

    adicionar ediçao de produto 1

--- api/produto-detalhes.js
+++ api/produto-detalhes.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { garantirTokenValido } from '../utils/refresh-ml.js'; // Importação correta
+import { garantirTokenValido } from './utils/refresh-ml.js'; // Importação correta
 
 export default async function handler(req, res) {
     const { id, usuarioId } = req.query;
